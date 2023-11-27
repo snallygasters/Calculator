89eee2870d594f52e964b7c8ee6044bfa8003289
Snallygasters <147277556+snallygasters@users.noreply.github.com>
2023-11-26 22:09:03 -0600

    EOD progress

--- app.js
+++ app.js
@@ -1,3 +1,28 @@
+const calculatorButtons = document.querySelector('.buttons-list');
+const displayCurrent = document.querySelector('.current-input')
+const decimal = document.querySelector('.decimal');
+const allClear = document.querySelector('#allclear');
+
+calculatorButtons.addEventListener('click', function(e) {
+    if (e.target.classList.contains('decimal') && e.target.classList.contains('input')) {
+        displayCurrent.textContent += e.target.textContent;
+        e.target.classList.remove('input');
+    }
+    
+    else if (e.target.classList.contains("input")) {
+        if (displayCurrent.textContent == '0') {
+            displayCurrent.textContent = e.target.textContent;
+        }
+        else {
+            displayCurrent.textContent += e.target.textContent;
+    }}
+});
+
+allClear.addEventListener('click', function() {
+    displayCurrent.textContent = '0';
+    decimal.classList.add('input');
+});
+
 function add() {
 
 }
