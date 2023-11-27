79bcf997ffcb92d66a84035ce823836e6e6156af
Snallygasters <147277556+snallygasters@users.noreply.github.com>
2023-11-26 20:51:18 -0600

    Initial calculator design

---
+++ app.js
@@ -0,0 +1,24 @@
+function add() {
+
+}
+
+function substract() {
+
+}
+
+function multiply() {
+
+}
+
+function divide() {
+
+}
+
+function operate() {
+    //takes an operator and 2 number and then calls an 
+    //above function
+}
+
+function display() {
+    //function that displays when buttons are clicked
+}
