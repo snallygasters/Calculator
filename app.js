54640559c9c9869f2fd7abb4974b1a92568becf4
Snallygasters <147277556+snallygasters@users.noreply.github.com>
2023-11-27 20:22:46 -0600

    first pass that meets specs

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const calculatorButtons = document.querySelector('.buttons-list');
 const displayCurrent = document.querySelector('.current-input')
 const decimal = document.querySelector('.decimal');
 const allClear = document.querySelector('#allclear');
+const displayPrevious = document.querySelector('.previous-input')
+const Equals = document.querySelector('.equals')
+const back = document.querySelector('#back');
 
 calculatorButtons.addEventListener('click', function(e) {
     if (e.target.classList.contains('decimal') && e.target.classList.contains('input')) {
@@ -15,35 +18,75 @@ calculatorButtons.addEventListener('click', function(e) {
         }
         else {
             displayCurrent.textContent += e.target.textContent;
-    }}
+    }
+    }
+    else if (e.target.classList.contains("operator")) {
+        if (displayCurrent.textContent !== "0") {
+            Equals.click();
+        }
+        displayPrevious.textContent = displayCurrent.textContent + " " + e.target.textContent;
+        displayCurrent.textContent = '0';
+        decimal.classList.add('input');
+    }
 });
 
 allClear.addEventListener('click', function() {
     displayCurrent.textContent = '0';
+    displayPrevious.textContent = ""
     decimal.classList.add('input');
 });
 
-function add() {
+back.addEventListener('click', function(){
+    displayCurrent.textContent = displayCurrent.textContent.slice(0,-1);
+})
 
+Equals.addEventListener('click', function(){
+    let splitNum = displayPrevious.textContent.split(" ");
+    operate(splitNum[0], displayCurrent.textContent, splitNum[1]);
+})
+
+function add(num1, num2) {
+    return displayCurrent.textContent = (parseFloat(num1) + parseFloat(num2));
 }
 
-function substract() {
-
+function subtract(num1, num2) {
+    return displayCurrent.textContent = (parseFloat(num1) - parseFloat(num2));
 }
 
-function multiply() {
-
+function multiply(num1, num2) {
+    return displayCurrent.textContent = (num1 * num2);
 }
 
-function divide() {
-
+function divide(num1, num2) {
+    if (num2 === '0') {
+        return displayCurrent.textContent = "REALLY BROTHER";
+    }
+    else {
+        return displayCurrent.textContent = (num1 / num2);
+    }
+    r
 }
 
-function operate() {
-    //takes an operator and 2 number and then calls an 
-    //above function
-}
+function operate(num1, num2, operator) {
+    switch (operator) {
+        case "+":
+        displayCurrent.textContent = add(num1, num2);
+        displayPrevious.textContent = "";
+        break;
 
-function display() {
-    //function that displays when buttons are clicked
+        case "-":
+        displayCurrent.textContent = subtract(num1, num2);
+        displayPrevious.textContent = "";
+        break;
+
+        case "*":
+        displayCurrent.textContent = multiply(num1, num2);
+        displayPrevious.textContent = "";
+        break;
+
+        case "/":
+        displayCurrent.textContent = divide(num1, num2);
+        displayPrevious.textContent = "";
+        break;
+    }
 }
